8a92fe02de35cfd88a7438ae2f76a8c398548e7b
i.ustinchenko <delfa07@bigmir.net>
2018-03-19 13:03:27 +0200

    002

--- src/components/Leagues.js
+++ src/components/Leagues.js
@@ -10,15 +10,28 @@ class Leagues extends Component {
 
     render() {
         const commands = this.props.commands;
+        const leagues = [];
+        const filterUnique = [];
+
+        commands.forEach((item, index) => {
+            filterUnique.push(item.league);
+
+            if (item.league.indexOf(filterUnique[index - 1]) !== -1) {
+                return;
+            }
+            leagues.push(
+                <li key={index}>
+                    {item.league}
+                </li>
+            );
+
+
+        });
 
         return (
             <section>
                 <h3>Leagues</h3>
-                {commands.map((item, index) =>
-                    <li key={index}>
-                        {item.league}
-                    </li>
-                )}
+                {leagues}
             </section>
         );
     }
