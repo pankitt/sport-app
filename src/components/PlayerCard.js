d9480c3fc4e886a349644f6de10c62581fead977
Kitt-PC <delfa07@bigmir.net>
2018-03-20 21:57:03 +0200

    fix card

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -2,30 +2,32 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class PlayerCard extends Component {
-    render() {
-        const num = this.props.obj.match.params.number;
-        const players = this.props.players;
-        const find = id => players.find(p => Number(p.id) === Number(id));
-        const item = find(num);
+  render() {
+    const num = this.props.obj.match.params.number;
+    const players = this.props.players;
+    const find = id => players.find(p => Number(p.id) === Number(id));
+    const item = find(num);
 
+    if (players.length === 0) return <div>Player not found</div>;
+    return (
+      <section>
+        <h3>User Card:</h3>
 
-        return (
-            <section>
-                <h3>User Card:</h3>
-
-                <div>
-                    <b>{item.name}</b>
-                    <ul>
-                        <li>command - {item.command}</li>
-                        <li>No - {item.No}</li>
-                        <li>Position - {item.Position}</li>
-                        <li>country - {item.country}</li>
-                    </ul>
-                </div>
-                <p><Link to='/players'>Back</Link></p>
-            </section>
-        );
-    }
+        <div>
+          <b>{item.name}</b>
+          <ul>
+            <li>command - {item.command}</li>
+            <li>No - {item.No}</li>
+            <li>Position - {item.Position}</li>
+            <li>country - {item.country}</li>
+          </ul>
+        </div>
+        <p>
+          <Link to="/players">Back</Link>
+        </p>
+      </section>
+    );
+  }
 }
 
 export default PlayerCard;
